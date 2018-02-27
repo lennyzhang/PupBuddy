045f567f42361f970f6e99a3f08ba7a8f8cc5dd1
lennyzhang <yunhengz@andrew.cmu.edu>
2018-02-27 16:13:16 -0500

    adds basic markups of frontend

---
+++ pupbuddy/webapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def login(request):
+    context = {}
+    return render(request, "login.html", context)
+
+def main(request):
+    context = {}
+    return render(request, "main.html", context)
