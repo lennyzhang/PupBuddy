b62d173c873741fc7053d1197b9c6a965981ba09
Xiaorui Li <xiaorui@Xiaoruis-MBP.fios-router.home>
2018-04-12 09:42:10 -0400

    update motorControl.py

--- pupbuddy/webapp/motorControl.py
+++ pupbuddy/webapp/motorControl.py
@@ -1,22 +1,75 @@
 import RPi.GPIO as GPIO
 import time
-#!/usr/bin/python
+
+import sys
+sys.path.append('/home/pi/Adafruit-Motor-HAT-Python-Library')
+
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+#https://media.readthedocs.org/pdf/adafruit-motor-hat/latest/adafruit-motor-hat.pdf
 import atexit
 
-# create a default object, no changes to I2C address or frequency
-mh = Adafruit_MotorHAT(addr=0x60)
+class PupBuddy:
+    """ CMU S18 24-671 Team:TBD PupBuddy """
+    def __init__(self):
+        # create a default object, no changes to I2C address or frequency
+        self.mh = Adafruit_MotorHAT(addr=0x60) # username, password ??
+        atexit.register(self.dcStop())
+
+        # DC motors - subject to changes
+        self.left = mh.getMotor(2)
+        self.right = mh.getMotor(1)
+        self.left.setSpeed(0)
+        self.right.setSpeed(0)
+        self.left.run(Adafruit_MotorHAT.RELEASE)
+        self.right.run(Adafruit_MotorHAT.RELEASE)
+        # launcher 
+        # treatLoader
 
 # recommended for auto-disabling motors on shutdown!
-def turnOffMotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-#    p.stop()
-    GPIO.cleanup()
+    def dcStop(self):
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    #    p.stop()
+        GPIO.cleanup()
 
-atexit.register(turnOffMotors)
+    def dcForward(self):
+        self.left.setSpeed(50)
+        self.right.setSpeed(50)
+        self.left.run(Adafruit_MotorHAT.FORWARD)
+        self.right.run(Adafruit_MotorHAT.FORWARD)
+
+    def dcForLeft(self):
+        self.left.setSpeed(25)
+        self.right.setSpeed(75)
+        self.self.left.run(Adafruit_MotorHAT.FORWARD)
+        self.right.run(Adafruit_MotorHAT.FORWARD)
+
+    def dcForRight(self):
+        self.left.setSpeed(75)
+        self.right.setSpeed(25)
+        self.left.run(Adafruit_MotorHAT.FORWARD)
+        self.right.run(Adafruit_MotorHAT.FORWARD)
+
+    def dcBackward(self):
+        self.left.setSpeed(30)
+        self.right.setSpeed(30)
+        self.left.run(Adafruit_MotorHAT.BACKWARD)
+        self.right.run(Adafruit_MotorHAT.BACKWARD)
+
+    def dcBackLeft(self):
+        self.left.setSpeed(25)
+        self.right.setSpeed(75)
+        self.left.run(Adafruit_MotorHAT.BACKWARD)
+        self.right.run(Adafruit_MotorHAT.BACKWARD)
+
+    def dcBackRight(self):
+        self.left.setSpeed(75)
+        self.right.setSpeed(25)
+        self.left.run(Adafruit_MotorHAT.BACKWARD)
+        self.right.run(Adafruit_MotorHAT.BACKWARD)
+
 
 
 #GPIO.setmode(GPIO.BOARD)
@@ -27,39 +80,6 @@ atexit.register(turnOffMotors)
 
 #p.start(7.5)
 
-
-################################# DC motor test!
-leftMotor = mh.getMotor(2)
-rightMotor = mh.getMotor(1)
-# set the speed to start, from 0 (off) to 255 (max speed)
-leftMotor.setSpeed(50)
-rightMotor.setSpeed(50)
-# turn on motor
-leftMotor.run(Adafruit_MotorHAT.RELEASE);
-rightMotor.run(Adafruit_MotorHAT.RELEASE);
-
-flag  = True
-
-
-
-for x in range(0,256,15):
-    print("x = ",x)
-    leftMotor.setSpeed(x)
-    rightMotor.setSpeed(x)
-    leftMotor.run(Adafruit_MotorHAT.FORWARD)
-    rightMotor.run(Adafruit_MotorHAT.BACKWARD)
-    time.sleep(3.0)
-    leftMotor.run(Adafruit_MotorHAT.RELEASE)
-    rightMotor.run(Adafruit_MotorHAT.RELEASE);
-    time.sleep(2.0)
-    print("Backward")
-    #leftMotor.run(Adafruit_MotorHAT.BACKWARD)
-    #rightMotor.run(Adafruit_MotorHAT.FORWARD)
-    #time.sleep(3.0)
-    #leftMotor.run(Adafruit_MotorHAT.RELEASE)
-    #rightMotor.run(Adafruit_MotorHAT.RELEASE);
-    #time.sleep(2.0)
-
     # servo
    # p.ChangeDutyCycle(7.5)  # turn towards 90 degree
     #time.sleep(1) # sleep 1 second
@@ -68,8 +88,5 @@ for x in range(0,256,15):
     #p.ChangeDutyCycle(12.5) # turn towards 180 degree
     #time.sleep(1) # sleep 1 second
     #p.stop() 
-    GPIO.cleanup()
-    flag = False;
-    print("Stop")
 
 
