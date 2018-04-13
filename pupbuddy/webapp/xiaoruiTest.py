c98173bafac0691bccfbf434b449ddcf7c799fc8
Xiaorui Li <xiaorui@Xiaoruis-MacBook-Pro.local>
2018-04-13 16:02:42 -0400

    Add xiaoruiTest.py for Local testing

---
+++ pupbuddy/webapp/xiaoruiTest.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('/home/pi/Desktop/webapp/Pupbuddy/pupbuddy/webapp')
+from motorControl import *
+import time
+
+robot = Pupbuddy()
+
+cmd = input("One Letter Command")
+print("you have endtered" + str(cmd))
+if(cmd == 'F'):
+	robot.dcForward()
+	time.sleep(2)
+else:
+	print("Nothing")
